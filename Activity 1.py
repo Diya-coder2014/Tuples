11330804344907825a37e4d81d40b8ffe95bf4f7
Diya-coder2014 <stu13311@dpsmisdoha.com>
2026-09-25 15:44:19 +0300

    Activity 1 done!

---
+++ Activity 1.py
@@ -0,0 +1,22 @@
+#1
+tuplex = (True, 10, "coding", 214.76)
+print(tuplex)
+
+#2
+tuplex = (2, 25, 46, 74, 324)
+print(tuplex)
+
+#3
+tuplex = (2, 5, 557, 35, 89, 23)
+tuplex = tuplex + (9,)
+print(tuplex)
+
+#4
+tuplex = (1, 65, 56, 67, 34, 23, 86, 56, 26, 56)
+tuplex = tuplex.count(56)
+print(tuplex)
+
+#5
+tupl1 = (1 ,34, 856, 86, "string", False, (13, 43, 14))
+slc = tupl1[3 : 10]
+print(slc)
